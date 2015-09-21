d1e2f8cf130cd513914831ca32cdc49c8877ef32
Aly <aschmidt36@my.wctc.edu>
2015-09-21 10:29:52 -0500

    Ran fine

--- src/dip/lab3/student/solution1/TipOfTheDayMessage.java
+++ src/dip/lab3/student/solution1/TipOfTheDayMessage.java
@@ -11,7 +11,7 @@ import java.util.Random;
 public class TipOfTheDayMessage implements InputService {
     private final String[] tips = {"Don't commit a crime while wearing a Fitbit.",
     "Tomatoes are fruit. They do not belong in fruit salad.", "One seventh"
-            + " of your life is speant on monday.", "You never truly understand"
+            + " of your life is spent on monday.", "You never truly understand"
             + " something until you can explain it to your Grandmother.", 
     "Stay out of the fire."};
     
